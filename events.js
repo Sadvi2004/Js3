5c199fe1433b1f800f9bb5663032b62274afb85c
sadvi134ash2004 <sadvibayyavarapu@gmail.com>
2025-01-25 10:36:03 +0530

    Added QR code generator functionality in qrgen.html, enhanced Dom.html with color containers, and updated event handling in events.js for mouse interactions.

--- events.js
+++ events.js
@@ -7,3 +7,13 @@ bt.addEventListener("click", () => {
         document.body.style.backgroundColor = "white";
     }, 2000)
 })
+
+let ele = document.querySelectorAll(".container");
+[...ele].map((elee) => {
+    elee.addEventListener("mouseover", () => {
+        elee.style.backgroundColor = elee.innerText;
+    });
+    elee.addEventListener("mouseleave", () => {
+        elee.style.backgroundColor = "transparent";
+    });
+});
